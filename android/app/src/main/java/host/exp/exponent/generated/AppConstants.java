e458e8ebda85345ed390f61dd3a83fc517dc2868
Dan Le Cornu <dan@noquarter.co>
2019-10-27 13:51:23 +0100

    random stuff

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -23,9 +23,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@nqdan/crow", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40nqdan%2Fcrow%2F1.0.2%2F6d13bc632721857eb2167702190d3d45-33.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
